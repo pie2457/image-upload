68817991071c8541b19e005405550c9c05b1fe93
pie <yejinh_h@naver.com>
2024-02-21 22:06:20 +0900

    CORS 설정

---
+++ src/main/java/image/yejin/infrastructure/config/WebConfig.java
@@ -0,0 +1,19 @@
+package image.yejin.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://13.124.30.105:8080")
+			.allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH");
+	}
+}
